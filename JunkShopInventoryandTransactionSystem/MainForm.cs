28a78c55691d2a2109179b3e764091067772b8dd
NellieLucky <arnelsgante@iskolarngbayan.pup.edu.ph>
2025-06-02 14:33:45 +0800

    test2

---
+++ JunkShopInventoryandTransactionSystem/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using JunkShopInventoryandTransactionSystem.View;
+
+namespace JunkShopInventoryandTransactionSystem
+{
+    public partial class MainForm : Form
+    {
+        public static Panel MainPanel;
+        public MainForm()
+        {
+            InitializeComponent();
+            MainPanel = panel1; // Assign the panel to a static variable for easy access
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LogInPage loginPage = new LogInPage();
+            loginPage.Dock = DockStyle.Fill;
+            loginPage.TopLevel = false;
+            panel1.Controls.Clear();
+            panel1.Controls.Add(loginPage);
+            loginPage.Show();
+        }
+    }
+}
--- JunkShopInventoryandTransactionSystem/View/LogInPage.Designer.cs
+++ JunkShopInventoryandTransactionSystem/View/LogInPage.Designer.cs
@@ -32,25 +32,25 @@
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogInPage));
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
-            LogIn_Panel = new JUNKSHOP.Resources.UserDefinedComponents.Gradient_Panel();
             Logo = new Guna.UI2.WinForms.Guna2CirclePictureBox();
-            SigInLabel = new Label();
-            EmailTextBox = new Guna.UI2.WinForms.Guna2TextBox();
-            EmailLabel = new Label();
+            LogIn_Panel = new JUNKSHOP.Resources.UserDefinedComponents.Gradient_Panel();
+            ForgotPasswordPageClick = new Label();
+            LogInButton = new Guna.UI2.WinForms.Guna2Button();
             PasswordLabel = new Label();
             PasswordTextBox = new Guna.UI2.WinForms.Guna2TextBox();
-            LogInButton = new Guna.UI2.WinForms.Guna2Button();
-            ForgotPasswordPageClick = new Label();
+            EmailLabel = new Label();
+            EmailTextBox = new Guna.UI2.WinForms.Guna2TextBox();
+            SigInLabel = new Label();
             guna2GradientPanel1.SuspendLayout();
-            LogIn_Panel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)Logo).BeginInit();
+            LogIn_Panel.SuspendLayout();
             SuspendLayout();
             // 
             // guna2GradientPanel1
@@ -67,6 +67,21 @@
             guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges9;
             guna2GradientPanel1.Size = new Size(1182, 753);
             guna2GradientPanel1.TabIndex = 0;
+            // 
+            // Logo
+            // 
+            Logo.BackColor = Color.FromArgb(0, 0, 0, 0);
+            Logo.BackgroundImageLayout = ImageLayout.Center;
+            Logo.Image = (Image)resources.GetObject("Logo.Image");
+            Logo.ImageRotate = 0F;
+            Logo.Location = new Point(76, 277);
+            Logo.Name = "Logo";
+            Logo.ShadowDecoration.CustomizableEdges = customizableEdges1;
+            Logo.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            Logo.Size = new Size(231, 217);
+            Logo.SizeMode = PictureBoxSizeMode.CenterImage;
+            Logo.TabIndex = 1;
+            Logo.TabStop = false;
             // 
             // LogIn_Panel
             // 
@@ -92,65 +107,36 @@
             LogIn_Panel.Size = new Size(791, 753);
             LogIn_Panel.TabIndex = 0;
             // 
-            // Logo
+            // ForgotPasswordPageClick
             // 
-            Logo.BackColor = Color.FromArgb(0, 0, 0, 0);
-            Logo.BackgroundImageLayout = ImageLayout.Center;
-            Logo.Image = (Image)resources.GetObject("Logo.Image");
-            Logo.ImageRotate = 0F;
-            Logo.Location = new Point(76, 277);
-            Logo.Name = "Logo";
-            Logo.ShadowDecoration.CustomizableEdges = customizableEdges1;
-            Logo.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            Logo.Size = new Size(231, 217);
-            Logo.SizeMode = PictureBoxSizeMode.CenterImage;
-            Logo.TabIndex = 1;
-            Logo.TabStop = false;
+            ForgotPasswordPageClick.AutoSize = true;
+            ForgotPasswordPageClick.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ForgotPasswordPageClick.ForeColor = Color.FromArgb(16, 107, 23);
+            ForgotPasswordPageClick.Location = new Point(259, 604);
+            ForgotPasswordPageClick.Name = "ForgotPasswordPageClick";
+            ForgotPasswordPageClick.Size = new Size(287, 35);
+            ForgotPasswordPageClick.TabIndex = 6;
+            ForgotPasswordPageClick.Text = "Forgot Password ?";
+            ForgotPasswordPageClick.Click += ForgotPasswordPageClick_Click;
             // 
-            // SigInLabel
+            // LogInButton
             // 
-            SigInLabel.AutoSize = true;
-            SigInLabel.Font = new Font("Arial", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            SigInLabel.ForeColor = Color.FromArgb(16, 107, 23);
-            SigInLabel.Location = new Point(83, 103);
-            SigInLabel.Name = "SigInLabel";
-            SigInLabel.Size = new Size(181, 55);
-            SigInLabel.TabIndex = 0;
-            SigInLabel.Text = "Sign In";
-            // 
-            // EmailTextBox
-            // 
-            EmailTextBox.BorderColor = Color.FromArgb(16, 107, 23);
-            EmailTextBox.BorderRadius = 30;
-            EmailTextBox.BorderThickness = 3;
-            EmailTextBox.CustomizableEdges = customizableEdges6;
-            EmailTextBox.DefaultText = "";
-            EmailTextBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            EmailTextBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            EmailTextBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            EmailTextBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            EmailTextBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            EmailTextBox.Font = new Font("Arial", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            EmailTextBox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            EmailTextBox.Location = new Point(95, 230);
-            EmailTextBox.Margin = new Padding(6, 6, 6, 6);
-            EmailTextBox.Name = "EmailTextBox";
-            EmailTextBox.PlaceholderText = "Enter your email...";
-            EmailTextBox.SelectedText = "";
-            EmailTextBox.ShadowDecoration.CustomizableEdges = customizableEdges7;
-            EmailTextBox.Size = new Size(617, 79);
-            EmailTextBox.TabIndex = 1;
-            // 
-            // EmailLabel
-            // 
-            EmailLabel.AutoSize = true;
-            EmailLabel.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            EmailLabel.ForeColor = Color.FromArgb(16, 107, 23);
-            EmailLabel.Location = new Point(95, 188);
-            EmailLabel.Name = "EmailLabel";
-            EmailLabel.Size = new Size(104, 35);
-            EmailLabel.TabIndex = 2;
-            EmailLabel.Text = "Email:";
+            LogInButton.AutoRoundedCorners = true;
+            LogInButton.CustomizableEdges = customizableEdges2;
+            LogInButton.DisabledState.BorderColor = Color.DarkGray;
+            LogInButton.DisabledState.CustomBorderColor = Color.DarkGray;
+            LogInButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            LogInButton.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            LogInButton.FillColor = Color.FromArgb(7, 96, 14);
+            LogInButton.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LogInButton.ForeColor = SystemColors.Window;
+            LogInButton.Location = new Point(95, 510);
+            LogInButton.Name = "LogInButton";
+            LogInButton.ShadowDecoration.CustomizableEdges = customizableEdges3;
+            LogInButton.Size = new Size(617, 79);
+            LogInButton.TabIndex = 5;
+            LogInButton.Text = "LOGIN";
+            LogInButton.Click += LogInButton_Click;
             // 
             // PasswordLabel
             // 
@@ -186,34 +172,50 @@
             PasswordTextBox.Size = new Size(617, 79);
             PasswordTextBox.TabIndex = 3;
             // 
-            // LogInButton
+            // EmailLabel
             // 
-            LogInButton.AutoRoundedCorners = true;
-            LogInButton.CustomizableEdges = customizableEdges2;
-            LogInButton.DisabledState.BorderColor = Color.DarkGray;
-            LogInButton.DisabledState.CustomBorderColor = Color.DarkGray;
-            LogInButton.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            LogInButton.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            LogInButton.FillColor = Color.FromArgb(7, 96, 14);
-            LogInButton.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            LogInButton.ForeColor = SystemColors.Window;
-            LogInButton.Location = new Point(95, 510);
-            LogInButton.Name = "LogInButton";
-            LogInButton.ShadowDecoration.CustomizableEdges = customizableEdges3;
-            LogInButton.Size = new Size(617, 79);
-            LogInButton.TabIndex = 5;
-            LogInButton.Text = "LOGIN";
+            EmailLabel.AutoSize = true;
+            EmailLabel.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            EmailLabel.ForeColor = Color.FromArgb(16, 107, 23);
+            EmailLabel.Location = new Point(95, 188);
+            EmailLabel.Name = "EmailLabel";
+            EmailLabel.Size = new Size(104, 35);
+            EmailLabel.TabIndex = 2;
+            EmailLabel.Text = "Email:";
             // 
-            // ForgotPasswordPageClick
+            // EmailTextBox
             // 
-            ForgotPasswordPageClick.AutoSize = true;
-            ForgotPasswordPageClick.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            ForgotPasswordPageClick.ForeColor = Color.FromArgb(16, 107, 23);
-            ForgotPasswordPageClick.Location = new Point(259, 604);
-            ForgotPasswordPageClick.Name = "ForgotPasswordPageClick";
-            ForgotPasswordPageClick.Size = new Size(287, 35);
-            ForgotPasswordPageClick.TabIndex = 6;
-            ForgotPasswordPageClick.Text = "Forgot Password ?";
+            EmailTextBox.BorderColor = Color.FromArgb(16, 107, 23);
+            EmailTextBox.BorderRadius = 30;
+            EmailTextBox.BorderThickness = 3;
+            EmailTextBox.CustomizableEdges = customizableEdges6;
+            EmailTextBox.DefaultText = "";
+            EmailTextBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            EmailTextBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            EmailTextBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            EmailTextBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            EmailTextBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            EmailTextBox.Font = new Font("Arial", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            EmailTextBox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            EmailTextBox.Location = new Point(95, 230);
+            EmailTextBox.Margin = new Padding(6);
+            EmailTextBox.Name = "EmailTextBox";
+            EmailTextBox.PlaceholderText = "Enter your email...";
+            EmailTextBox.SelectedText = "";
+            EmailTextBox.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            EmailTextBox.Size = new Size(617, 79);
+            EmailTextBox.TabIndex = 1;
+            // 
+            // SigInLabel
+            // 
+            SigInLabel.AutoSize = true;
+            SigInLabel.Font = new Font("Arial", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SigInLabel.ForeColor = Color.FromArgb(16, 107, 23);
+            SigInLabel.Location = new Point(83, 103);
+            SigInLabel.Name = "SigInLabel";
+            SigInLabel.Size = new Size(181, 55);
+            SigInLabel.TabIndex = 0;
+            SigInLabel.Text = "Sign In";
             // 
             // LogInPage
             // 
@@ -221,12 +223,13 @@
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1182, 753);
             Controls.Add(guna2GradientPanel1);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "LogInPage";
             Text = "Junk Shop Inventory and Transaction Management System";
             guna2GradientPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)Logo).EndInit();
             LogIn_Panel.ResumeLayout(false);
             LogIn_Panel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)Logo).EndInit();
             ResumeLayout(false);
         }
 
