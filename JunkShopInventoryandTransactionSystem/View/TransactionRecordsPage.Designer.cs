02f297ab2de4dbed98a5fc20cddac376446f0137
Sandara Fillartos <sandaragfillartos@iskolarngbayan.pup.edu.ph>
2025-06-14 02:12:28 +0800

    transaction records changes

--- JunkShopInventoryandTransactionSystem/View/TransactionRecordsPage.Designer.cs
+++ JunkShopInventoryandTransactionSystem/View/TransactionRecordsPage.Designer.cs
@@ -29,35 +29,20 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            cuiLabel4 = new CuoreUI.Controls.cuiLabel();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             cuiColorPicker1 = new CuoreUI.Components.cuiColorPicker(components);
-            cuiPanel1 = new CuoreUI.Controls.cuiPanel();
-            tableLayoutPanel2 = new TableLayoutPanel();
-            label1 = new Label();
-            tableLayoutPanel1 = new TableLayoutPanel();
-            cuiLabel6 = new CuoreUI.Controls.cuiLabel();
-            cuiLabel5 = new CuoreUI.Controls.cuiLabel();
-            cuiLabel3 = new CuoreUI.Controls.cuiLabel();
-            cuiLabel2 = new CuoreUI.Controls.cuiLabel();
-            cuiLabel1 = new CuoreUI.Controls.cuiLabel();
             DashboardTitlePage = new Label();
-            cuiScrollbar1 = new CuoreUI.Controls.cuiScrollbar();
-            cuiPanel1.SuspendLayout();
-            tableLayoutPanel1.SuspendLayout();
+            dataGridView1 = new DataGridView();
+            Date = new DataGridViewTextBoxColumn();
+            ItemName = new DataGridViewTextBoxColumn();
+            Category = new DataGridViewTextBoxColumn();
+            QtyType = new DataGridViewTextBoxColumn();
+            TransacQuantity = new DataGridViewTextBoxColumn();
+            ExchangeAmount = new DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // cuiLabel4
-            // 
-            cuiLabel4.BackColor = Color.Transparent;
-            cuiLabel4.Content = "Item\\ Name";
-            cuiLabel4.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cuiLabel4.HorizontalAlignment = StringAlignment.Center;
-            cuiLabel4.Location = new Point(605, 6);
-            cuiLabel4.Margin = new Padding(5, 6, 5, 6);
-            cuiLabel4.Name = "cuiLabel4";
-            cuiLabel4.Size = new Size(202, 86);
-            cuiLabel4.TabIndex = 7;
-            cuiLabel4.VerticalAlignment = StringAlignment.Center;
             // 
             // cuiColorPicker1
             // 
@@ -65,207 +50,139 @@
             cuiColorPicker1.EnableThemeChangeButton = true;
             cuiColorPicker1.Theme = CuoreUI.Components.Forms.ColorPickerForm.Themes.Light;
             // 
-            // cuiPanel1
-            // 
-            cuiPanel1.AutoScroll = true;
-            cuiPanel1.BackColor = SystemColors.GrayText;
-            cuiPanel1.Controls.Add(tableLayoutPanel2);
-            cuiPanel1.Controls.Add(label1);
-            cuiPanel1.Controls.Add(tableLayoutPanel1);
-            cuiPanel1.ForeColor = Color.FromArgb(5, 84, 11);
-            cuiPanel1.Location = new Point(19, 132);
-            cuiPanel1.Margin = new Padding(2);
-            cuiPanel1.Name = "cuiPanel1";
-            cuiPanel1.OutlineThickness = 1F;
-            cuiPanel1.PanelColor = Color.FromArgb(227, 244, 238);
-            cuiPanel1.PanelOutlineColor = Color.White;
-            cuiPanel1.Rounding = new Padding(8);
-            cuiPanel1.Size = new Size(1294, 820);
-            cuiPanel1.TabIndex = 6;
-            // 
-            // tableLayoutPanel2
-            // 
-            tableLayoutPanel2.BackColor = Color.FromArgb(250, 250, 250);
-            tableLayoutPanel2.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
-            tableLayoutPanel2.ColumnCount = 6;
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 66.6666641F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 235F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 209F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 232F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 209F));
-            tableLayoutPanel2.Location = new Point(24, 161);
-            tableLayoutPanel2.Margin = new Padding(2);
-            tableLayoutPanel2.Name = "tableLayoutPanel2";
-            tableLayoutPanel2.RowCount = 6;
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50.4717F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 49.5283F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 105F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 99F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 102F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Absolute, 95F));
-            tableLayoutPanel2.Size = new Size(1252, 636);
-            tableLayoutPanel2.TabIndex = 4;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(374, 260);
-            label1.Margin = new Padding(2, 0, 2, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(0, 25);
-            label1.TabIndex = 3;
-            // 
-            // tableLayoutPanel1
-            // 
-            tableLayoutPanel1.BackColor = Color.FromArgb(163, 244, 215);
-            tableLayoutPanel1.ColumnCount = 6;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30.6233063F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 69.37669F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 234F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 212F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 231F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 208F));
-            tableLayoutPanel1.Controls.Add(cuiLabel6, 5, 0);
-            tableLayoutPanel1.Controls.Add(cuiLabel5, 4, 0);
-            tableLayoutPanel1.Controls.Add(cuiLabel4, 3, 0);
-            tableLayoutPanel1.Controls.Add(cuiLabel3, 2, 0);
-            tableLayoutPanel1.Controls.Add(cuiLabel2, 1, 0);
-            tableLayoutPanel1.Controls.Add(cuiLabel1, 0, 0);
-            tableLayoutPanel1.Location = new Point(24, 15);
-            tableLayoutPanel1.Margin = new Padding(2);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 1;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 48.68421F));
-            tableLayoutPanel1.Size = new Size(1252, 109);
-            tableLayoutPanel1.TabIndex = 2;
-            // 
-            // cuiLabel6
-            // 
-            cuiLabel6.BackColor = Color.Transparent;
-            cuiLabel6.Content = "Payment\\ Method";
-            cuiLabel6.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cuiLabel6.HorizontalAlignment = StringAlignment.Center;
-            cuiLabel6.Location = new Point(1048, 6);
-            cuiLabel6.Margin = new Padding(5, 6, 5, 6);
-            cuiLabel6.Name = "cuiLabel6";
-            cuiLabel6.Size = new Size(196, 86);
-            cuiLabel6.TabIndex = 9;
-            cuiLabel6.VerticalAlignment = StringAlignment.Center;
-            // 
-            // cuiLabel5
-            // 
-            cuiLabel5.BackColor = Color.Transparent;
-            cuiLabel5.Content = "Total\\ Amount";
-            cuiLabel5.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cuiLabel5.HorizontalAlignment = StringAlignment.Center;
-            cuiLabel5.Location = new Point(817, 6);
-            cuiLabel5.Margin = new Padding(5, 6, 5, 6);
-            cuiLabel5.Name = "cuiLabel5";
-            cuiLabel5.Size = new Size(221, 86);
-            cuiLabel5.TabIndex = 8;
-            cuiLabel5.VerticalAlignment = StringAlignment.Center;
-            // 
-            // cuiLabel3
-            // 
-            cuiLabel3.BackColor = Color.Transparent;
-            cuiLabel3.Content = "Item\\ Type";
-            cuiLabel3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cuiLabel3.HorizontalAlignment = StringAlignment.Center;
-            cuiLabel3.Location = new Point(371, 6);
-            cuiLabel3.Margin = new Padding(5, 6, 5, 6);
-            cuiLabel3.Name = "cuiLabel3";
-            cuiLabel3.Size = new Size(224, 86);
-            cuiLabel3.TabIndex = 6;
-            cuiLabel3.VerticalAlignment = StringAlignment.Center;
-            // 
-            // cuiLabel2
-            // 
-            cuiLabel2.BackColor = Color.Transparent;
-            cuiLabel2.Content = "Customer\\ Type:\\ Supplier\\ \\n\\|\\ Buyer";
-            cuiLabel2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cuiLabel2.HorizontalAlignment = StringAlignment.Center;
-            cuiLabel2.Location = new Point(117, 6);
-            cuiLabel2.Margin = new Padding(5, 6, 5, 6);
-            cuiLabel2.Name = "cuiLabel2";
-            cuiLabel2.Size = new Size(244, 94);
-            cuiLabel2.TabIndex = 5;
-            cuiLabel2.VerticalAlignment = StringAlignment.Center;
-            // 
-            // cuiLabel1
-            // 
-            cuiLabel1.BackColor = Color.Transparent;
-            cuiLabel1.Content = "Date";
-            cuiLabel1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            cuiLabel1.HorizontalAlignment = StringAlignment.Center;
-            cuiLabel1.Location = new Point(5, 6);
-            cuiLabel1.Margin = new Padding(5, 6, 5, 6);
-            cuiLabel1.Name = "cuiLabel1";
-            cuiLabel1.Size = new Size(102, 94);
-            cuiLabel1.TabIndex = 4;
-            cuiLabel1.VerticalAlignment = StringAlignment.Center;
-            // 
             // DashboardTitlePage
             // 
             DashboardTitlePage.AutoSize = true;
             DashboardTitlePage.BackColor = Color.FromArgb(0, 0, 0, 0);
-            DashboardTitlePage.Font = new Font("Arial", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            DashboardTitlePage.ForeColor = Color.FromArgb(16, 107, 23);
+            DashboardTitlePage.Font = new Font("Arial", 18F, FontStyle.Bold);
+            DashboardTitlePage.ForeColor = Color.FromArgb(0, 157, 209);
             DashboardTitlePage.Location = new Point(19, 28);
             DashboardTitlePage.Margin = new Padding(4, 0, 4, 0);
             DashboardTitlePage.Name = "DashboardTitlePage";
-            DashboardTitlePage.Size = new Size(500, 56);
+            DashboardTitlePage.Size = new Size(373, 43);
             DashboardTitlePage.TabIndex = 5;
             DashboardTitlePage.Text = "Transaction Records";
             // 
-            // cuiScrollbar1
+            // dataGridView1
             // 
-            cuiScrollbar1.Background = Color.Transparent;
-            cuiScrollbar1.HoveredThumbColor = Color.FromArgb(192, 255, 106, 0);
-            cuiScrollbar1.Location = new Point(1319, 129);
-            cuiScrollbar1.Margin = new Padding(2);
-            cuiScrollbar1.MinimumSize = new Size(20, 50);
-            cuiScrollbar1.Name = "cuiScrollbar1";
-            cuiScrollbar1.PressedThumbColor = Color.FromArgb(192, 255, 106, 0);
-            cuiScrollbar1.Rounding = 5;
-            cuiScrollbar1.Size = new Size(20, 852);
-            cuiScrollbar1.TabIndex = 7;
-            cuiScrollbar1.TargetControl = null;
-            cuiScrollbar1.ThumbColor = Color.FromArgb(255, 106, 0);
-            cuiScrollbar1.ThumbHeight = 50;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.SunkenHorizontal;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(163, 244, 215);
+            dataGridViewCellStyle1.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(163, 244, 215);
+            dataGridViewCellStyle1.SelectionForeColor = Color.Black;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeight = 60;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Date, ItemName, Category, QtyType, TransacQuantity, ExchangeAmount });
+            dataGridView1.EditMode = DataGridViewEditMode.EditProgrammatically;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.GridColor = Color.FromArgb(99, 99, 99);
+            dataGridView1.Location = new Point(19, 75);
+            dataGridView1.Margin = new Padding(4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 50;
+            dataGridViewCellStyle3.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle3.ForeColor = Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = Color.FromArgb(0, 157, 209);
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.RowTemplate.Height = 50;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(1320, 850);
+            dataGridView1.TabIndex = 31;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            // 
+            // Date
+            // 
+            Date.HeaderText = "Item Name";
+            Date.MinimumWidth = 6;
+            Date.Name = "Date";
+            Date.Width = 200;
+            // 
+            // ItemName
+            // 
+            ItemName.HeaderText = "Category";
+            ItemName.MinimumWidth = 6;
+            ItemName.Name = "ItemName";
+            ItemName.Width = 200;
+            // 
+            // Category
+            // 
+            Category.FillWeight = 150F;
+            Category.HeaderText = "Qty Type";
+            Category.MinimumWidth = 6;
+            Category.Name = "Category";
+            Category.Width = 220;
+            // 
+            // QtyType
+            // 
+            QtyType.FillWeight = 125F;
+            QtyType.HeaderText = "Exchange Quantity";
+            QtyType.MinimumWidth = 6;
+            QtyType.Name = "QtyType";
+            QtyType.Width = 250;
+            // 
+            // TransacQuantity
+            // 
+            TransacQuantity.FillWeight = 59.177578F;
+            TransacQuantity.HeaderText = "Exchange Amount";
+            TransacQuantity.MinimumWidth = 6;
+            TransacQuantity.Name = "TransacQuantity";
+            TransacQuantity.Width = 250;
+            // 
+            // ExchangeAmount
+            // 
+            ExchangeAmount.FillWeight = 59.177578F;
+            ExchangeAmount.HeaderText = "Delete";
+            ExchangeAmount.Image = Properties.Resources.delete;
+            ExchangeAmount.MinimumWidth = 6;
+            ExchangeAmount.Name = "ExchangeAmount";
+            ExchangeAmount.Resizable = DataGridViewTriState.True;
+            ExchangeAmount.SortMode = DataGridViewColumnSortMode.Automatic;
+            ExchangeAmount.Width = 210;
             // 
             // TransactionRecordsPage
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(227, 244, 238);
-            Controls.Add(cuiPanel1);
+            Controls.Add(dataGridView1);
             Controls.Add(DashboardTitlePage);
-            Controls.Add(cuiScrollbar1);
             Margin = new Padding(4, 4, 4, 4);
             Name = "TransactionRecordsPage";
             Size = new Size(1355, 998);
-            cuiPanel1.ResumeLayout(false);
-            cuiPanel1.PerformLayout();
-            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private CuoreUI.Controls.cuiLabel cuiLabel4;
         private CuoreUI.Components.cuiColorPicker cuiColorPicker1;
-        private CuoreUI.Controls.cuiPanel cuiPanel1;
-        private TableLayoutPanel tableLayoutPanel2;
-        private Label label1;
-        private TableLayoutPanel tableLayoutPanel1;
-        private CuoreUI.Controls.cuiLabel cuiLabel6;
-        private CuoreUI.Controls.cuiLabel cuiLabel5;
-        private CuoreUI.Controls.cuiLabel cuiLabel3;
-        private CuoreUI.Controls.cuiLabel cuiLabel2;
-        private CuoreUI.Controls.cuiLabel cuiLabel1;
         private Label DashboardTitlePage;
-        private CuoreUI.Controls.cuiScrollbar cuiScrollbar1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Date;
+        private DataGridViewTextBoxColumn ItemName;
+        private DataGridViewTextBoxColumn Category;
+        private DataGridViewTextBoxColumn QtyType;
+        private DataGridViewTextBoxColumn TransacQuantity;
+        private DataGridViewImageColumn ExchangeAmount;
     }
 }
